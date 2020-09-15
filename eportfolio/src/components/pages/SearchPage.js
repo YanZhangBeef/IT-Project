d83ac9fe0606f39b416820c7bfee5cdd21c24dcd
Muhammad Haikal Abdullah Zain <muhammadhaik@student.unimelb.edu.au>
2020-09-15 17:29:12 +1000

    revert changes to search

--- eportfolio/src/components/pages/SearchPage.js
+++ eportfolio/src/components/pages/SearchPage.js
@@ -1,23 +1,21 @@
 import React from "react";
 import ProjectResults from "../search/ProjectResults";
 import PeopleResults from "../search/PeopleResults";
-import "./SearchPage.module.css";
-
-import styles from "./Chat.module.css";
+import "./SearchPage.css";
 
 const search = () => (
-  <div class={styles["search-container"]}>
+  <div class="container">
     <h1>Projects</h1>
 
     <ProjectResults />
-    <div className={styles.more}>
+    <div className="more">
       <p>Load more projects</p>
       <hr></hr>
     </div>
     <h1>People</h1>
 
     <PeopleResults />
-    <div className={styles.more}>
+    <div className="more">
       <p>Load more people</p>
       <hr></hr>
     </div>
