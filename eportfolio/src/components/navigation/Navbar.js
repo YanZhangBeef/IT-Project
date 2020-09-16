815b58adc59e3f97d02beabe72cc239922eccae1
vincent-haoy <53930677+vincent-haoy@users.noreply.github.com>
2020-09-17 01:48:24 +1000

    merge navbar

--- eportfolio/src/components/navigation/Navbar.js
+++ eportfolio/src/components/navigation/Navbar.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
-
+import Icon1 from "./img/MailIcon.png"
+import Icon2 from "./img/peopleIcon.png"
 export default function Navbar(props) {
   return (
     <nav className={`${styles["app-nav"]} navbar py-4 border-bottom shadow-sm`}>
-      <span>ePortfolio</span>
-      <Link className={styles.link} to="/login">
-        Login
-      </Link>
-      <Link className={styles.link} to="/chat">
-        Inbox
-      </Link>
+      <Link className={styles.link} to = "/login">ePortfolio</Link>
+
+      <div className = {styles.searchContainer}>
+          <input type="text" id = "inputtext" name="condition" placeholder="Search" />
+          <input type="submit" id ="bottom" value="search" />
+      </div>
+
+
+      {/**The PPL and mail Icon */}
+      <ul className={styles.navbaricon}>
+        <li className={styles.icon}> 
+          <Link to = "/chat"><img src={Icon1} alt="MailIcon"/></Link>
+        </li>
+        <li className={styles.icon}> 
+          <Link to = "/"><img src={Icon2} alt="PeopleIcon"/></Link>
+        </li>
+      </ul>
     </nav>
   );
 }
