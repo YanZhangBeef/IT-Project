8f5e318ac122eb14296a1e4b336eeccf21488d38
vincent-haoy <53930677+vincent-haoy@users.noreply.github.com>
2020-09-23 18:21:50 +1000

    massive clean up
    using modules.css only & delete the pngs & fix the indentations

--- eportfolio/src/components/navigation/Navbar.js
+++ eportfolio/src/components/navigation/Navbar.js
@@ -1,40 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import navbarStyles from './Navbar.module.css';
 import {
   faMailBulk,
   faUser,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
-import "./Navbar.css";
+
 export default function Navbar(props) {
   return (
-<nav class="navbar navbar-expand-xl navbar-light" id = "navbar">
-    <a class="navbar-brand" id = "eport">ePortfolio</a>
+    <nav className={`navbar navbar-expand-xl navbar-light ${navbarStyles.navbar}`}>
+      <a class = "navbar-brand"
+      style = {{marginLeft: "2rem", color:"white"}}
+       >ePortfolio</a>
 
-
-  <div class="container" id = "search">
-  <form class="form-inline">
-    <div class="input-group">
-      <div class="input-group-prepend">
-      <FontAwesomeIcon icon={ faSearch} />
-      </div>
-      <input type="text" class="form-control" id="searchinput" placeholder="search from unimelb students" aria-describedby="basic-addon1"/>
+      <div className={`d-flex align-items-center" ${navbarStyles.searchbarContainer}`}>
+        <form class="form-inline">
+          <div class="input-group">
+            <div  className={`input-group-prepend ${navbarStyles.inputGroupPrepend}`}>
+              <FontAwesomeIcon icon={ faSearch}/>
+            </div>
+            <input type="text" class="form-control" 
+            style = {{border: "0ch", backgroundColor: "#96bbe8",width: "18rem"}}
+            placeholder="search from unimelb students" 
+            aria-describedby="basic-addon1"/>
+          </div>
+        </form>
     </div>
-  </form>
-</div>
 
-
-
-     <div class="container-fluid" id = "icons">
-    <div class = "circle" id = "circle">
-      <a class="navbar-brand" href="/chat" id = "MailBulk"><FontAwesomeIcon icon={faMailBulk} id="imailbulk"/></a> 
-    </div>
-    <div class = "circle" id = "circle">
-    <a class="navbar-brand" href="/" id = "user"><FontAwesomeIcon icon={ faUser} id = "iuser" /></a>
-    </div>
-    <a class="btn btn-outline-light" href="/login" role="button">login</a>
-    </div>
-</nav>
+      <a className={`navbar-brand ${navbarStyles.iconsHolder}`}>
+        <div class="d-flex align-items-center">
+          <div className = {navbarStyles.circle}>
+            <a class="navbar-brand" href="/chat" ><FontAwesomeIcon icon={faMailBulk} className={`${navbarStyles.mailIcon}`}/></a> 
+          </div>
+          <div className = {navbarStyles.circle}>
+            <a class="navbar-brand" href="/"><FontAwesomeIcon icon={ faUser} className={`${navbarStyles.peopleIcon}`} /></a>
+          </div>
+          <a class="btn btn-outline-light" href="/login" role="button">login</a>
+        </div>
+      </a>
+    </nav>
   )
 }
