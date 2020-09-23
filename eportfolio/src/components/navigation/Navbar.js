187df0db53a92cf92ddea12bd00b5d1c947d91a9
vincent-haoy <53930677+vincent-haoy@users.noreply.github.com>
2020-09-23 18:32:24 +1000

    adding comments

--- eportfolio/src/components/navigation/Navbar.js
+++ eportfolio/src/components/navigation/Navbar.js
@@ -9,17 +9,21 @@ import {
 
 export default function Navbar(props) {
   return (
-    <nav className={`navbar navbar-expand-xl navbar-light ${navbarStyles.navbar}`}>
-      <a class = "navbar-brand"
-      style = {{marginLeft: "2rem", color:"white"}}
-       >ePortfolio</a>
 
+    <nav className={`navbar navbar-expand-xl navbar-light ${navbarStyles.navbar}`}>
+      <a class = "navbar-brand"style = {{marginLeft: "2rem", color:"white"}}>ePortfolio</a>
+
+      {/**the search bar module*/}
       <div className={`d-flex align-items-center" ${navbarStyles.searchbarContainer}`}>
         <form class="form-inline">
           <div class="input-group">
+            
+            {/**search icon */}
             <div  className={`input-group-prepend ${navbarStyles.inputGroupPrepend}`}>
               <FontAwesomeIcon icon={ faSearch}/>
             </div>
+
+            {/**search input */}
             <input type="text" class="form-control" 
             style = {{border: "0ch", backgroundColor: "#96bbe8",width: "18rem"}}
             placeholder="search from unimelb students" 
@@ -28,14 +32,19 @@ export default function Navbar(props) {
         </form>
     </div>
 
+      {/**Icon module*/}
       <a className={`navbar-brand ${navbarStyles.iconsHolder}`}>
+        {/**Mail Icon*/}
         <div class="d-flex align-items-center">
           <div className = {navbarStyles.circle}>
             <a class="navbar-brand" href="/chat" ><FontAwesomeIcon icon={faMailBulk} className={`${navbarStyles.mailIcon}`}/></a> 
           </div>
+
+          {/**User Icon*/}
           <div className = {navbarStyles.circle}>
             <a class="navbar-brand" href="/"><FontAwesomeIcon icon={ faUser} className={`${navbarStyles.peopleIcon}`} /></a>
           </div>
+          {/**log in button */}
           <a class="btn btn-outline-light" href="/login" role="button">login</a>
         </div>
       </a>
