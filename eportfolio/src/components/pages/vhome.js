cf2c89d2f954d6e9e36e4e33aa47b018f82d9fc8
vincent-haoy <53930677+vincent-haoy@users.noreply.github.com>
2020-09-27 03:04:03 +1000

    adding comments
    adding comment && stylings

--- eportfolio/src/components/pages/vhome.js
+++ eportfolio/src/components/pages/vhome.js
@@ -10,43 +10,48 @@ faPeopleArrows
 
 export default function vhome(props) {
   return (
+    
     <div class = "container-fullwidth ">
 
-    <div className = {hompageStyle.home}>
-      <div className = {hompageStyle.landingtext}> 
-        <h1 className = {hompageStyle.hometext}>5 Head ePortfolio</h1>
-        <br></br>
-        <a class="btn btn-primary btn-lg" href="#" role="button"
-        style = {{background:"#90BBEF 71%",border: "0.15rem solid #FC5C39",
-        borderRadius: "0.8rem"}}>use for free </a>
+      {/**the full beach width picture  */}
+      <div className = {hompageStyle.home}>
+        <div className = {hompageStyle.landingtext}> 
+          <h1 className = {hompageStyle.hometext}>5 Head ePortfolio</h1>
+          <br></br>
+
+          {/**the buttom  */}
+          <a class="btn btn-primary btn-lg" href="#" role="button"
+          style = {{background:"#90BBEF 71%",border: "0.15rem solid #FC5C39",
+          borderRadius: "0.8rem"}}>use for free </a>
         </div>        
-        </div>
-        
+      </div>
+
+      {/**the 3 Icons */}  
       <footer class = "container-fluid text-center">
-      <div class = "row" style={{marginTop:"2rem"}}>
+        <div class = "row" style={{marginTop:"2rem"}}>
+
+        {/**the Upload Icon */}  
           <div class = "col-sm">
-          <a class="icon" href="#" ><FontAwesomeIcon icon={faCloudUploadAlt} className={`${hompageStyle.homeicons}`}/></a> 
-          <br></br><br></br>
-          <p className={`${hompageStyle.icontext}`}>Upload My Artifact</p>
+            <a class="icon" href="#" ><FontAwesomeIcon icon={faCloudUploadAlt} className={`${hompageStyle.homeicons}`}/></a> 
+            <br></br><br></br>
+            <p className={`${hompageStyle.icontext}`}>Upload My Artifact</p>
           </div>
 
-
+            {/**the get hired Icon */}  
           <div class = "col-sm">
-          <a class="icon" href="#" ><FontAwesomeIcon icon={faBriefcase} className={`${hompageStyle.homeicons}`}/></a> 
-          <br></br><br></br>
-          <p className={`${hompageStyle.icontext}`}>Get hired by employers</p>
+            <a class="icon" href="#" ><FontAwesomeIcon icon={faBriefcase} className={`${hompageStyle.homeicons}`}/></a> 
+            <br></br><br></br>
+            <p className={`${hompageStyle.icontext}`}>Get hired by employers</p>
           </div>
 
+          {/**the inviting ppl Icon */}  
           <div class = "col-sm">
-          <a class="icon" href="#" ><FontAwesomeIcon icon={faPeopleArrows} className={`${hompageStyle.homeicons}`}/></a> 
-          <br></br><br></br>
-          <p className={`${hompageStyle.icontext}`}>Share with friends</p>
+            <a class="icon" href="#" ><FontAwesomeIcon icon={faPeopleArrows} className={`${hompageStyle.homeicons}`}/></a> 
+            <br></br><br></br>
+            <p className={`${hompageStyle.icontext}`}>Share with friends</p>
           </div>
         </div>
-
-        
-
       </footer>
-        </div>
+    </div>
   );
 }
