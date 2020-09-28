53e5c828b5ee8064178a0ac49e2b361fbefe048f
Muhammad Haikal Abdullah Zain <muhammadhaik@student.unimelb.edu.au>
2020-09-28 18:30:44 +1000

    improved navbar

--- eportfolio/src/components/navigation/Navbar.js
+++ eportfolio/src/components/navigation/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import navbarStyles from './Navbar.module.css';
+import styles from "./Navbar.module.css";
 import {
   faMailBulk,
   faUser,
@@ -9,45 +9,57 @@ import {
 
 export default function Navbar(props) {
   return (
-    
-    <nav className={`navbar navbar-expand-md navbar-light ${navbarStyles.navbar}`}>
-      <a class = "collapse navbar-collapse"style = {{marginLeft: "2rem", marginRight:"20rem", color:"white"}}>ePortfolio</a>
+    <nav
+      className={`navbar navbar-dark navbar-expand-md justify-content-between navbar-light ${styles.navbar}`}
+    >
+      <a className="navbar-brand">ePortfolio</a>
 
       {/**the search bar module*/}
-      <div className={`d-flex align-items-center" ${navbarStyles.searchbarContainer}`}>
-        <form class="form-inline">
-          <div class="input-group">
-            
-            {/**search icon */}
-            <div  className={`input-group-prepend ${navbarStyles.inputGroupPrepend}`}>
-              <FontAwesomeIcon icon={ faSearch}/>
-            </div>
+      <div
+        className={`d-flex align-items-center mx-auto ${styles.searchbarContainer}`}
+      >
+        {/**search icon */}
+        <div className={`mx-2 ${styles.inputGroupPrepend}`}>
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
 
-            {/**search input */}
-            <input type="text" class="form-control" 
-            style = {{border: "0ch", backgroundColor: "#96bbe8"}}
-            placeholder="search from students" 
-            aria-describedby="basic-addon1"/>
-          </div>
-        </form>
-    </div>
+        {/**search input */}
+        <input
+          type="text"
+          className={`m-1 ${styles.searchBar}`}
+          placeholder="Search for projects"
+          aria-describedby="basic-addon1"
+        />
+      </div>
 
       {/**Icon module*/}
-      <a className={`collapse navbar-collapse ${navbarStyles.iconsHolder}`}>
+      <div className={`collapse navbar-collapse ${styles.iconsHolder}`}>
         {/**Mail Icon*/}
-        <div class="d-flex align-items-center">
-          <div className = {navbarStyles.circle}>
-            <a class="navbar-brand" href="/chat" ><FontAwesomeIcon icon={faMailBulk} className={`${navbarStyles.mailIcon}`}/></a> 
+        <div className="d-flex align-items-center">
+          <div className={styles.circle}>
+            <a class="navbar-brand" href="/chat">
+              <FontAwesomeIcon
+                icon={faMailBulk}
+                className={`${styles.mailIcon}`}
+              />
+            </a>
           </div>
 
           {/**User Icon*/}
-          <div className = {navbarStyles.circle}>
-            <a class="navbar-brand" href="/"><FontAwesomeIcon icon={ faUser} className={`${navbarStyles.peopleIcon}`} /></a>
+          <div className={styles.circle}>
+            <a class="navbar-brand" href="/">
+              <FontAwesomeIcon
+                icon={faUser}
+                className={`${styles.peopleIcon}`}
+              />
+            </a>
           </div>
           {/**log in button */}
-          <a class="btn btn-outline-light" href="/login" role="button">login</a>
+          <a class="btn btn-outline-light" href="/login" role="button">
+            login
+          </a>
         </div>
-      </a>
+      </div>
     </nav>
-  )
+  );
 }
