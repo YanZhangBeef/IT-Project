f03aca6be93c276bd9e55dad369ad12d921a5746
G M Asif Tareque <gtareque@student.unimelb.edu.au>
2020-09-28 11:18:58 +1000

    complete search backend with vincent's navbar

--- eportfolio/src/components/navigation/Navbar.js
+++ eportfolio/src/components/navigation/Navbar.js
@@ -15,75 +15,71 @@ const Navbar = (props) => {
   }
 
   return (
-    <div className={navbarStyles.outerNav}>
-      <nav
-        className={`navbar navbar-expand-md navbar-light fixed-top  ${navbarStyles.navbar}`}
+    <nav className={`navbar navbar-expand-md ${navbarStyles.navbar}`}>
+      <a
+        class="collapse navbar-collapse"
+        style={{ marginLeft: "2rem", marginRight: "20rem", color: "white" }}
       >
-        <a
-          class="collapse navbar-collapse"
-          style={{ marginLeft: "2rem", marginRight: "20rem", color: "white" }}
-        >
-          ePortfolio
-        </a>
+        ePortfolio
+      </a>
 
-        {/**the search bar module*/}
-        <div
-          className={`d-flex align-items-center" ${navbarStyles.searchbarContainer}`}
-        >
-          <form class="form-inline">
-            <div class="input-group">
-              {/**search icon */}
-              <div
-                className={`input-group-prepend ${navbarStyles.inputGroupPrepend}`}
-              >
-                <FontAwesomeIcon icon={faSearch} />
-              </div>
+      {/**the search bar module*/}
+      <div
+        className={`d-flex align-items-center" ${navbarStyles.searchbarContainer}`}
+      >
+        <form class="form-inline">
+          <div class="input-group">
+            {/**search icon */}
+            <div
+              className={`input-group-prepend ${navbarStyles.inputGroupPrepend}`}
+            >
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
 
-              {/**search input */}
-              <input
-                type="text"
-                name="query"
-                id="searchQuery"
-                class="form-control"
-                style={{ border: "0ch", backgroundColor: "#96bbe8" }}
-                placeholder="search from students"
-                onChange={handleChange}
-                aria-describedby="basic-addon1"
+            {/**search input */}
+            <input
+              type="text"
+              name="query"
+              id="searchQuery"
+              class="form-control"
+              style={{ border: "0ch", backgroundColor: "#96bbe8" }}
+              placeholder="search from students"
+              onChange={handleChange}
+              aria-describedby="basic-addon1"
+            />
+          </div>
+        </form>
+      </div>
+
+      {/**Icon module*/}
+      <a className={`collapse navbar-collapse ${navbarStyles.iconsHolder}`}>
+        {/**Mail Icon*/}
+        <div class="d-flex align-items-center">
+          <div className={navbarStyles.circle}>
+            <a class="navbar-brand" href="/chat">
+              <FontAwesomeIcon
+                icon={faMailBulk}
+                className={`${navbarStyles.mailIcon}`}
               />
-            </div>
-          </form>
-        </div>
-
-        {/**Icon module*/}
-        <a className={`collapse navbar-collapse ${navbarStyles.iconsHolder}`}>
-          {/**Mail Icon*/}
-          <div class="d-flex align-items-center">
-            <div className={navbarStyles.circle}>
-              <a class="navbar-brand" href="/chat">
-                <FontAwesomeIcon
-                  icon={faMailBulk}
-                  className={`${navbarStyles.mailIcon}`}
-                />
-              </a>
-            </div>
-
-            {/**User Icon*/}
-            <div className={navbarStyles.circle}>
-              <a class="navbar-brand" href="/">
-                <FontAwesomeIcon
-                  icon={faUser}
-                  className={`${navbarStyles.peopleIcon}`}
-                />
-              </a>
-            </div>
-            {/**log in button */}
-            <a class="btn btn-outline-light" href="/login" role="button">
-              login
             </a>
           </div>
-        </a>
-      </nav>
-    </div>
+
+          {/**User Icon*/}
+          <div className={navbarStyles.circle}>
+            <a class="navbar-brand" href="/">
+              <FontAwesomeIcon
+                icon={faUser}
+                className={`${navbarStyles.peopleIcon}`}
+              />
+            </a>
+          </div>
+          {/**log in button */}
+          <a class="btn btn-outline-light" href="/login" role="button">
+            login
+          </a>
+        </div>
+      </a>
+    </nav>
   );
 };
 
