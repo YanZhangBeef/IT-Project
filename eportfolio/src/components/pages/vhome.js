94ed466b39b3c1cc4df665638ee70de055fb3bf2
vincent-haoy <53930677+vincent-haoy@users.noreply.github.com>
2020-09-28 17:21:44 +1000

    adding dummy  paragraph & fixing the button

--- eportfolio/src/components/pages/vhome.js
+++ eportfolio/src/components/pages/vhome.js
@@ -19,38 +19,46 @@ export default function vhome(props) {
           <br></br>
 
           {/**the buttom  */}
-          <a class="btn btn-primary btn-lg" href="#" role="button"
-          style = {{background:"#90BBEF 71%",border: "0.15rem solid #FC5C39",
-          borderRadius: "0.8rem"}}>use for free </a>
+          <a class="btn btn-outline-light btn-lg" href="#" role="button">use for free </a>
         </div>        
       </div>
 
       {/**the 3 Icons */}  
-      <footer class = "container-fluid text-center">
+      <div class = "container-fluid text-center">
         <div class = "row" style={{marginTop:"2rem"}}>
 
         {/**the Upload Icon */}  
-          <div class = "col-sm">
+          <div class = "col-md">
             <a class="icon" href="#" ><FontAwesomeIcon icon={faCloudUploadAlt} className={`${hompageStyle.homeicons}`}/></a> 
             <br></br><br></br>
             <p className={`${hompageStyle.icontext}`}>Upload My Artifact</p>
+            <p  className={`${hompageStyle.iconparagraph}`}>you can post your artifacts ,picture and videos</p>
+            
+            <p  className={`${hompageStyle.iconparagraph}`}>You can also add more specific description about your artifacts, like
+          dates, titles and introduction</p>
           </div>
 
             {/**the get hired Icon */}  
-          <div class = "col-sm">
+          <div class = "col-md">
             <a class="icon" href="#" ><FontAwesomeIcon icon={faBriefcase} className={`${hompageStyle.homeicons}`}/></a> 
             <br></br><br></br>
             <p className={`${hompageStyle.icontext}`}>Get hired by employers</p>
+            <p  className={`${hompageStyle.iconparagraph}`}>explore the employers</p>
+            <p  className={`${hompageStyle.iconparagraph}`}>The employers can also read your artifacts and they can send you
+            a message if they want to hire you</p>
           </div>
 
           {/**the inviting ppl Icon */}  
-          <div class = "col-sm">
+          <div class = "col-md">
             <a class="icon" href="#" ><FontAwesomeIcon icon={faPeopleArrows} className={`${hompageStyle.homeicons}`}/></a> 
             <br></br><br></br>
             <p className={`${hompageStyle.icontext}`}>Share with friends</p>
+            <p  className={`${hompageStyle.iconparagraph}`}>Copy our link and share it with friends</p>
+            <p  className={`${hompageStyle.iconparagraph}`}>You can share your artifact to certain group of your friends as per
+            your preference privacy settings</p>
           </div>
         </div>
-      </footer>
+      </div>
     </div>
   );
 }
