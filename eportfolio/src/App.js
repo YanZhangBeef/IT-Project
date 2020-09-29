90462ff7c676da103a8cf1baca6c1352394b87d0
TechAustraliaPartener <50567866+TechAustraliaPartener@users.noreply.github.com>
2020-09-29 21:18:42 +1000

    used usecontext

--- eportfolio/src/App.js
+++ eportfolio/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import AuthRoute from "./data/AuthRoute";
@@ -14,58 +14,44 @@ import Login from "./components/pages/Login";
 import Home from "./components/pages/Home";
 
 import "./data/firebase";
-import { auth } from "./data/firebase";
 
 import { fakeProfile, fakeContent } from "./TestData";
 import Chat from "./components/pages/Chat";
-
+import { AuthProvider } from "./data/Auth";
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const unlisten = auth().onAuthStateChanged((user) => {
-      user ? setAuthenticated(true) : setAuthenticated(false);
-    });
-    return () => {
-      unlisten();
-    };
-  }, []);
   return (
     <React.Fragment>
-      <Navbar />
-      <Switch>
-        <Route path="/newContent/:userId/:sectionId">
-          <CreateContentPageContainer />
-        </Route>
-        <Route path="/editContent/:contentId">
-          <ContentPageContainer isEditing={true} />
-        </Route>
-        <Route path="/content/:contentId">
-          <ContentPageContainer />
-        </Route>
-        <Route path="/profile/:userId">
-          <ProfilePageContainer />
-        </Route>
-        <Route exact path="/content">
-          <ContentPage {...fakeContent} />
-        </Route>
+      <AuthProvider>
+        <Navbar />
+        <Switch>
+          <Route path="/newContent/:userId/:sectionId">
+            <CreateContentPageContainer />
+          </Route>
+          <Route path="/editContent/:contentId">
+            <ContentPageContainer isEditing={true} />
+          </Route>
+          <Route path="/content/:contentId">
+            <ContentPageContainer />
+          </Route>
+          <Route path="/profile/:userId">
+            <ProfilePageContainer />
+          </Route>
+          <Route exact path="/content">
+            <ContentPage {...fakeContent} />
+          </Route>
 
-        <Route exact path="/">
-          <ProfilePage {...fakeProfile} />
-        </Route>
-        <AuthRoute
-          exact
-          path="/chat"
-          authenticated={authenticated}
-          component={Chat}
-        />
-        <Route path="/search">
-          <SearchPage />
-        </Route>
+          <Route exact path="/">
+            <ProfilePage {...fakeProfile} />
+          </Route>
+          <AuthRoute exact path="/chat" component={Chat} />
+          <Route path="/search">
+            <SearchPage />
+          </Route>
 
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/home" component={Home} />
-      </Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </AuthProvider>
     </React.Fragment>
   );
 }
--- eportfolio/src/components/pages/Login.js
+++ eportfolio/src/components/pages/Login.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import styles from "./Login.module.css";
 import { Redirect } from "react-router-dom";
+import { AuthContext } from "../../data/Auth";
 
 const Login = (props) => {
-  const [user, setUser] = useState(null);
   let uiConfig = {
     signInFlow: "popup",
     signInOptions: [
@@ -19,32 +19,25 @@ const Login = (props) => {
     },
   };
 
-  useEffect(() => {
-    const unlisten = firebase.auth().onAuthStateChanged((loggedIn) => {
-      loggedIn ? setUser(loggedIn) : setUser(null);
-    });
-    return () => {
-      unlisten();
-    };
-  }, []);
+  const { currentUser } = useContext(AuthContext);
+
+  if (currentUser) {
+    return <Redirect to="/home" />;
+  }
   return (
     <div className={styles["login-container"]}>
-      {user ? (
-        <Redirect to="/home" />
-      ) : (
-        <div className={`${styles["login"]} shadow`}>
-          <div className={styles["login-method"]}>
-            <div className={styles["login-header"]}>
-              <h2>Log In</h2>
-            </div>
-            <StyledFirebaseAuth
-              uiConfig={uiConfig}
-              firebaseAuth={firebase.auth()}
-            />
+      <div className={`${styles["login"]} shadow`}>
+        <div className={styles["login-method"]}>
+          <div className={styles["login-header"]}>
+            <h2>Log In</h2>
           </div>
-          <div className={styles["app-icon"]}></div>
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
         </div>
-      )}
+        <div className={styles["app-icon"]}></div>
+      </div>
     </div>
   );
 };
