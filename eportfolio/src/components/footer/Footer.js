cdb8707e09c15d00c0972f450e3c359611720d99
Muhammad Haikal Abdullah Zain <muhammadhaik@student.unimelb.edu.au>
2020-09-30 17:46:59 +1000

    fixed footer

--- eportfolio/src/components/footer/Footer.js
+++ eportfolio/src/components/footer/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
-import footerStyles from './Footer.module.css';
+import footerStyles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-<footer className={`navbar sticky-bottom navbar-dark ${footerStyles.footer}`}>
-<a class="navbar-brand" href = "https://github.com/vincent-haoy/IT-Project">Contact us</a>
-<a class="navbar-brand" style = {{fontSize:"x-small"}}>copyright@2020@github</a>
-<a class="navbar-brand" href = "https://itproject-5head.atlassian.net/wiki/spaces/IP/overview?homepageId=393218">5heads</a>
-</footer>
-  )
+    <footer
+      className={`navbar p-3 justify-content-center bg-light sticky-bottom ${footerStyles.footer}`}
+    >
+      <span className={footerStyles.copy}>Copyright &copy;5Head 2020</span>
+    </footer>
+  );
 }
